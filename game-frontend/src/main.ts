e506c59832d7a5f9ecbcfbe57131aceb35f523ac
Dominik Matt <mail@matt-dominik.at>
2018-10-01 21:50:53 +0200

    Update main.ts

--- game-frontend/src/main.ts
+++ game-frontend/src/main.ts
@@ -46,7 +46,6 @@ app.on("window-all-closed", () => {
     }
 });
 
-
 app.on("activate", () => {
     // On OS X it"s common to re-create a window in the app when the
     // dock icon is clicked and there are no other windows open.
