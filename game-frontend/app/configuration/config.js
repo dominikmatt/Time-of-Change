58e8066b884d4c0350a003c99f3c5f436dae650d
Dominik Matt <mail@matt-dominik.at>
2019-01-18 12:41:14 +0100

    fixed dist

---
+++ game-frontend/app/configuration/config.js
@@ -0,0 +1,34 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+let instance = null;
+class Config {
+    static getInstance() {
+        if (null === instance) {
+            instance = new this();
+        }
+        return instance;
+    }
+    constructor() {
+        let env = 'prod';
+        if (process.env.ELECTRON_ENV) {
+            env = process.env.ELECTRON_ENV;
+        }
+        this._config = require(`${__dirname}/config.${env}`).default;
+    }
+    get(key, defaultValue = '') {
+        if (!this.exists(key)) {
+            return defaultValue;
+        }
+        return this._config[key];
+    }
+    exists(key) {
+        if (this._config[key]) {
+            return true;
+        }
+        return false;
+    }
+}
+exports.Config = Config;
+const config = Config.getInstance();
+exports.default = config;
+//# sourceMappingURL=config.js.map
--- game-frontend/src/configuration/config.prod.ts
+++ game-frontend/src/configuration/config.prod.ts
@@ -7,3 +7,4 @@ export default {
         'debug': false,
     }
 };
+
